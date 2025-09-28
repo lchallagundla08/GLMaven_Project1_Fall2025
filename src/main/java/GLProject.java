4d14cc915b1221d4e7f3e838c1199163aba2e5dd
Sanjana Challagundla <137904660+lchallagundla08@users.noreply.github.com>
2025-09-28 18:05:37 -0500

    commit to pull

--- src/main/java/GLProject.java
+++ src/main/java/GLProject.java
@@ -6,16 +6,7 @@ public class GLProject {
 		// TODO Auto-generated method stub
 		//System.out.println("Welcome to project 1");
         // Test MyHashMap
-        System.out.println("\n=== Testing MyHashMap ===");
-        MyHashMap<String> map = new MyHashMap<>("key1", "value1");
-        map.put("key2", "value2");
-        map.put("Cozmo", "robotValue"); // This should hash to index 0
 
-        System.out.println("Size: " + map.size());
-        System.out.println("Contains 'key1': " + map.contains("key1"));
-        System.out.println("Get 'Cozmo': " + map.get("Cozmo"));
-        System.out.println("Replace 'key1': " + map.replace("key1", "newValue1"));
-        System.out.println("Get 'key1' after replace: " + map.get("key1"));
 
     }
 }
